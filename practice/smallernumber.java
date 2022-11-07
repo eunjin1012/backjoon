824b161a7ad65367f44b2ac6c56423b29610fce6
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-11-07 23:29:17 +0900

    smallernumber

---
+++ practice/smallernumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int X=sc.nextInt();
+        
+        for(int i=1; i<N+1; i++)
+        {
+            int A=sc.nextInt();
+            if(X>A)
+            {
+                System.out.print(A+" ");
+            }
+        }
+    }
+}
