58c89f09a241e84141375d01590a208df4bf2e9d
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-12-11 17:39:43 +0900

    3003

---
+++ practice/3003.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr = new int[6];
+        int[] set = {1,1,2,2,2,8};
+        for(int i=0; i<6; i++){
+            arr[i] = sc.nextInt();
+            System.out.print(set[i]-arr[i]+" ");
+        }
+    }
+}
