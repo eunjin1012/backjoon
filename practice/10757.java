81fe8b49a7665efa7005cf19d2e46cfd2ea09271
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-12-12 21:55:57 +0900

    10757

---
+++ practice/10757.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+        System.out.println(a.add(b));
+    }
+}
