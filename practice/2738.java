2d717007e21c8588ade34b0559e1cfc71e57e7a4
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-12-14 16:41:26 +0900

    2738

---
+++ practice/2738.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] arr = new int[N][M];
+        
+        for(int i=0; i<2; i++){
+            for(int j=0; j<N; j++){
+                for(int h=0; h<M; h++){
+                arr[j][h]+=sc.nextInt();
+                }
+            }
+        }
+        for(int k=0; k<N; k++){
+            for(int m=0; m<M; m++){
+                System.out.print(arr[k][m]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
