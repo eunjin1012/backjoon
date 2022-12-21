31b306c2b1d8c3516df574285b248f0c684324a9
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-12-21 16:33:27 +0900

    1436

---
+++ practice/1436.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 1;
+        int first = 666;
+        while(count!=n){
+            first++;
+        if(String.valueOf(first).contains("666")){
+            count++;
+        }
+      }
+        System.out.println(first);
+    }
