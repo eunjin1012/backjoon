617f1c00b11115542e7727d942e29faee1a036ef
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-12-23 22:38:56 +0900

    1676

---
+++ practice/1676.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while(n>=5){
+            count+=n/5;
+            n=n/5;
+        }
+        System.out.println(count);
+    }
+} 
