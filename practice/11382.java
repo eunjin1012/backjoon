01cf19f639f5b78eacda305a9e94b258043d31b0
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2023-02-22 17:17:07 +0900

    11382

---
+++ practice/11382.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long k = sc.nextLong();
+        System.out.println(n+m+k);
+    }
+}
